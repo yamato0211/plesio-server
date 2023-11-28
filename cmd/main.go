ed45e927f536c72d9680c703111f70039ecb2f4e
yamato0211 <kumayama0211@gmail.com>
2023-11-28 23:16:29 +0900

    change: ciのテスト

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -12,6 +14,15 @@ func main() {
 
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
+
+	num, err := strconv.Atoi("1")
+	if err != nil {
+		fmt.Println("hoge")
+	}
+	fmt.Println(num)
+
+	num, err = strconv.Atoi("2")
+	fmt.Println(num)
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "hello")
